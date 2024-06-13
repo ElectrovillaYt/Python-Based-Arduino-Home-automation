48d6c91b1415c5931034bc274618587b69529c4b
ElectrovillaYt <electrovillaofficial@gmail.com>
2024-06-13 15:07:29 +0530

    Update voice_assistant.py

--- voice_assistant.py
+++ voice_assistant.py
@@ -1,11 +1,15 @@
 #Name of the assistant is Jarvis
 import speech_recognition as sr
 import win32com.client
-import serial
-import time
+import pyfirmata
+import datetime
 
-ser=serial.Serial('com3',9600)
-time.sleep(0.5)
+comport='COM3'
+board=pyfirmata.Arduino(comport)
+out_1 = board.get_pin('d:2:o')
+out_2 = board.get_pin('d:3:o')
+out_3 = board.get_pin('d:4:o')
+out_4 = board.get_pin('d:5:o')
 
 spk = win32com.client.Dispatch("SAPI.SpVoice")
 
@@ -16,8 +20,10 @@ def voice_input():
         aud = recognizer.listen(source)
         try:
             query = recognizer.recognize_google(aud, language="en-US")
-            # print(query)
+            print(query)
             return query
+        except sr.UnknownValueError:
+            return "Sorry, didn't get that!"
         except Exception as e:
             return " "
 def recognize():
@@ -25,46 +31,58 @@ def recognize():
         query = voice_input()
         if query.lower() == "turn on led 1":
             spk.speak("Okay, Turning led 1 ON")
-            ser.write(b'1')
+            out_1.write(1)
         elif query.lower() == "turn off led 1":
             spk.speak("Okay, Turning led 1 OFF")
-            ser.write(b'2')
+            out_1.write(0)
         
         if query.lower() == "turn on led 2":
             spk.speak("Okay, Turning led 2 ON")
-            ser.write(b'3')
+            out_2.write(1)
         elif query.lower() == "turn off led 2":
             spk.speak("Okay, Turning led 2 OFF")
-            ser.write(b'4')
+            out_2.write(0)
         
         if query.lower() == "turn on led 3":
             spk.speak("Okay, Turning led 3 ON")
-            ser.write(b'5')
+            out_3.write(1)
         elif query.lower() == "turn off led 3":
             spk.speak("Okay, Turning led 3 OFF")
-            ser.write(b'6')
+            out_3.write(0)
         
         if query.lower() == "turn on led 4":
             spk.speak("Okay, Turning led 4 ON")
-            ser.write(b'7')
+            out_4.write(1)
         elif query.lower() == "turn off led 4":
             spk.speak("Okay, Turning led 4 OFF")
-            ser.write(b'8')
+            out_4.write(0)
+
+        elif query.lower() == "turn on all leds":
+            spk.speak("Okay, Turning all leds ON")
+            out_1.write(1)
+            out_2.write(1)
+            out_3.write(1)
+            out_4.write(1)    
         elif query.lower() == "turn off all leds":
             spk.speak("Okay, Turning all leds OFF")
-            ser.write(b'9')
+            out_1.write(0)
+            out_2.write(0)
+            out_3.write(0)
+            out_4.write(0)
 
-        if query == "exit":
-            time.sleep(2)
+        elif query.lower() == "what's the time":
+            x=datetime.datetime.now()
+            spk.speak(f"The time is {x.strftime("%I %M %p")}")
+        elif query.lower() == "what's the date today":
+            x=datetime.datetime.now()
+            spk.speak(f"Today is {x.strftime("%B")},{x.day}")
+
+        elif query.lower() == "bye":
             spk.speak("Goodbye")
             break
 
 while True:
     query = voice_input()
     if query.lower() == "hey jarvis":
-        spk.speak("Hi there, How can I assist you today?")
+        spk.speak(f"Hi there, How can I assist you today?")
         recognize()
-    if query == "exit":
-        time.sleep(2)
-        spk.speak("Goodbye")
-        break
